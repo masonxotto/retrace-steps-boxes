731bc7b174ba05f317faaa752259866e563ec656
masonxotto <masonxotto@gmail.com>
2022-09-23 14:15:43 -0500

    added variable sizeOfBoxes to change depending on how many divs with class="box"

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-document.addEventListener('click', boxListener);
 
 let boxStack = [];
 let boxInterval;
+const sizeOfGrid = document.getElementsByClassName("box").length;
+console.log(sizeOfGrid);
+
 
 function boxListener(event) {
     if (!event.target.classList.contains("box")) return;
     if(boxStack.includes(event.target)) return;
     boxStack.push(event.target);
     event.target.classList.add("clicked");
-    if(boxStack.length >= 9) {
+    if(boxStack.length >= sizeOfGrid) {
             boxInterval = setInterval( () => {
                     boxStack[0].classList.remove("clicked");
                     boxStack.shift(); 
@@ -17,6 +19,7 @@ function boxListener(event) {
     }
 }
 
+document.addEventListener('click', boxListener);
 
 
 
